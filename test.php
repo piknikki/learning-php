636efc52a7d9a45e7a66612ba77fa02ad3d6fac3
Nikki Petersen <piknik@me.com>
2019-08-01 08:59:05 -0600

    adding other conditions to an if else

--- test.php
+++ test.php
@@ -19,5 +19,30 @@ echo $_SERVER['HTTP_USER_AGENT'];
 <h3>This is an h3 in regular html</h3>
 
 
+<?php
+if (strpos($_SERVER['HTTP_USER_AGENT'], 'Mozilla') !== FALSE) {
+    echo 'You are using Mozilla <br />';
+} else {
+    echo 'You are not cool <br />';
+}
+?>
+
+<p>strpos() is a function built into PHP which searches a string for another string</p>
+
+
+<?php
+if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE) {
+    ?>
+    <h3>strpos() must have returned non-false</h3>
+    <p>You are using Internet Explorer</p>
+    <?php
+} else {
+    ?>
+    <h3>strpos() must have returned false </h3>
+    <p>You are NOT using Internet Explorer</p>
+    <?php
+}
+?>
+
 </body>
 </html>

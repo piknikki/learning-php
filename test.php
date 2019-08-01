40e29123096acd8bd078d84c8e8c6605cef37101
Nikki Petersen <piknik@me.com>
2019-08-01 08:45:25 -0600

    learning about variables

---
+++ test.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP test</title>
+</head>
+<body>
+<h1>Hello this is the h1</h1>
+
+<?php echo '<p> Hello World </p>'; ?>
+
+<?php
+echo $_SERVER['HTTP_USER_AGENT'];
+?>
+
+<h3>This is an h3 in regular html</h3>
+
+
+</body>
+</html>
